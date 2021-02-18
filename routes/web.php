f36f7c845138d08e8dd4f166a57f4382f87b77df
williamtruppi <williamt@hotmail.it>
2021-02-18 17:07:03 +0100

    route e PageController creati

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@home')->name('home');
+Route::get('/about', 'PageController@about')->name('about');
+Route::resource("blog", "PostController");
