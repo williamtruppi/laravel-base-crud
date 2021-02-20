2ac7244b026f7b084de354ba3108149a33d546cf
williamtruppi <williamt@hotmail.it>
2021-02-20 16:54:49 +0100

    metodo delete completato - segue refactoring e stile

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -91,6 +91,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('posts.index');
     }
 }
