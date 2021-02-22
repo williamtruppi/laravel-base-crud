c1febfe4191bc985b102df856247cecc8b67a01d
williamtruppi <williamt@hotmail.it>
2021-02-22 10:25:57 +0100

    implementata direttiva error per i singoli campi del form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -23,13 +23,18 @@
         @csrf
         <div class="form-group">
             <label for="title">Title</label>
-            <input class="form-control" type="text" name="title" id="title">
+            <input class="form-control" type="text" name="title" id="title" value="{{old('title')}}">
         </div>
-
+        @error('title')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
         <div class="form-group">
             <label for="body">Body</label>
-            <textarea class="form-control" name="body" id="body" rows="3"></textarea>
+            <textarea class="form-control" name="body" id="body" rows="3">{{old('body')}}</textarea>
         </div>
+        @error('body')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
         <button type="submit" class="btn btn-primary">Submit</button>
 
     </form>
